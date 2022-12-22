9192984a555ac90de6cffa7475c1772d73daaacc
unknown <you@example.com>
2022-12-22 11:50:55 -0800

    This is my work

---
+++ web/table.js
@@ -0,0 +1,17 @@
+// for(i=2;i<=20;i=i+2)
+// {
+//     console.log(i)
+// }
+// for(i=3;i<=30;i=i+3)
+// {
+//     console.log(i)
+
+
+    var result='\n';
+    for (var i=1; i<11; i++){
+        for(var j=1; j<11;j++){
+            result +=(i*j) + '';
+        }
+    
+    result +='\n';
+}
